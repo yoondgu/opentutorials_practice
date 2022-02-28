14299c5c66bf8ff90f74d230e1ca61f8f33fa3e5
yoondgu <doityoungg@gmail.com>
2022-02-28 18:40:01 +0900

    method  정의 및 출력 이용하여 main 코드 단순화

--- JAVA/myapp/src/account_app.java
+++ JAVA/myapp/src/account_app.java
@@ -1,31 +1,36 @@
 
 public class account_app {
+	
+	public static double valueOfsupply = 10000;
+	public static double vatrate = 0.1;
+	public static double expenseRate = 0.3;
+	public static double[] dividend = {0.5, 0.3, 0.2};
+	
+	public static double getVAT() {
+		return valueOfsupply * vatrate;
+	}
+	public static double getTotal() {
+		return valueOfsupply + getVAT();
+	}
+	public static double getExpense() {
+		return valueOfsupply * expenseRate;
+	}
+	public static double getIncome() {
+		return valueOfsupply - getExpense();
+	}
+	public static double getDividend(int i) {
+		return getIncome() * dividend[i];
+	}
 
-	public static void main(String[] args) {
-		
-		double valueOfsupply = Double.parseDouble(args[0]);
-		double vatrate = 0.1;
-		double expenseRate = 0.3;
-
-		double vat = valueOfsupply * vatrate;
-		double total = valueOfsupply + vat;
-		double expense = valueOfsupply * expenseRate;
-		double income = valueOfsupply - expense;
-		double dividend1 = income * 0.5;
-		double dividend2 = income * 0.3;
-		double dividend3 = income * 0.2;
-		
-		
+	public static void main(String[] args) {		
 		System.out.println("Value of supply: " + valueOfsupply);
-		System.out.println("VAT: " + vat);
-		System.out.println("Total: " + total);
-		System.out.println("Expense: " + expense);
-		System.out.println("Income: " + income);
-		System.out.println("Dividend: " + dividend1);
-		System.out.println("Dividend: " + dividend2);
-		System.out.println("Dividend: " + dividend3);
-		
-		
+		System.out.println("VAT: " + getVAT());
+		System.out.println("Total: " + getTotal());
+		System.out.println("Expense: " + getExpense());
+		System.out.println("Income: " + getIncome());
+		System.out.println("Dividend1: " + getDividend(0));
+		System.out.println("Dividend2: " + getDividend(1));
+		System.out.println("Dividend3: " + getDividend(2));
 	}
 
 }
